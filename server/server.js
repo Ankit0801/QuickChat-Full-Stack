c5eee1cf11c2a25c7bbe60cef6c33a2524681bb1
Ankit Sarma <63030019+Ankit0801@users.noreply.github.com>
2025-06-09 00:39:42 +0530

    Update server.js
    Changed cors

--- server/server.js
+++ server/server.js
@@ -38,11 +38,11 @@ io.on("connection", (socket)=>{
 
 // Middleware setup
 app.use(express.json({limit: "4mb"}));
-// app.use(cors());
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
+app.use(cors());
+// app.use(cors({
+//   origin: "http://localhost:5173",
+//   credentials: true
+// }));
 
 
 // Routes setup
